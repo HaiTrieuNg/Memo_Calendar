f3ca35305fd1b3bdca353873d1cea02c3e4f3b85
opalharshe <47206352+opalharshe@users.noreply.github.com>
2019-05-09 00:12:59 -0700

    Update TaskFrame.java

--- TaskFrame.java
+++ TaskFrame.java
@@ -23,6 +23,9 @@ class TaskFrame extends JFrame implements ActionListener
 	int ret;
 	JLabel Day;
 	public String dayofFrame;
+	private JComboBox cb1;
+	private JComboBox cb2;
+	private JComboBox cb3;
 	TaskFrame(String day, int d, String month, int year)
 	{
 		dayofFrame=day;
@@ -35,6 +38,7 @@ class TaskFrame extends JFrame implements ActionListener
 		setLayout(new BoxLayout(c, BoxLayout.Y_AXIS));
 		
 		JPanel all = new JPanel();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		all.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 3));
 		all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
 		
@@ -50,21 +54,41 @@ class TaskFrame extends JFrame implements ActionListener
 		tasks = new Task();
 		
 		list = new JList (tasks.getTask());
-		
+		Color skyBlue = new Color(235,250,250);
+		list.setBackground(skyBlue);
 		list.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2));
 		
-		 
 		JScrollPane js = new JScrollPane(list);
 	
-		js.setPreferredSize(new Dimension(24,100));
+		js.setPreferredSize(new Dimension(300,400));
+		js.getViewport().setBackground(Color.gray);
 		all.add(js);
+	      
+	    // Put the combo boxes for Time into the panel.
+	      
+	    String Hr[]={"0","1","2","3","4","5","6","7","8","9","10","11","12"};        
+	    String Min[]={":00",":15",":30",":45"};        
+	    String AMPM[]={"am","pm"};   
+	    cb1=new JComboBox(Hr);    
+	    cb2=new JComboBox(Min);    
+	    cb3=new JComboBox(AMPM);    
+	    JPanel time = new JPanel();
+		time.setLayout(new FlowLayout(FlowLayout.LEFT));
+		time.add(new JLabel("Time"));
+	    time.add(cb1);
+	    time.add(cb2);
+	    time.add(cb3);
+	    all.add(time);
 		
-		//Creating Text Field
-		JPanel text = new JPanel();
-		textfield = new JTextField(24);  
-		text.add(textfield);
-		all.add(text);
-		
+		//Creating Text Field for Task.
+
+	    JPanel task = new JPanel();
+		task.setLayout(new FlowLayout(FlowLayout.LEFT));
+		task.add(new JLabel("Task"));
+		textfield = new JTextField(20); 
+		task.add(textfield);
+		all.add(task);
+
 		//Creating Buttons
 		AddBtn = new JButton("Add");
 		AddBtn.addActionListener(this);
@@ -86,8 +110,9 @@ class TaskFrame extends JFrame implements ActionListener
 		add(all);
 		all.add(buttonPane);
 		
-		setSize(300,250);
-		//setVisible(true);
+		setSize(300,500);
+		setVisible(false);
+		
 		}
 		
 		public void actionPerformed(ActionEvent ae)
@@ -96,7 +121,7 @@ class TaskFrame extends JFrame implements ActionListener
 		{
 			if (tasks.getTask().getSize()>0)
 			{
-				String select = textfield.getText();
+				String select = cb1.getSelectedItem().toString()+cb2.getSelectedItem().toString()+cb3.getSelectedItem().toString()+" - "+textfield.getText();
 				Object obj = select;
 				ret = list.getSelectedIndex();
 				tasks.remove(ret);
@@ -119,7 +144,8 @@ class TaskFrame extends JFrame implements ActionListener
 		else if ("Add".equals(ae.getActionCommand()))
 		{
 		
-			String select = textfield.getText();
+			String select = cb1.getSelectedItem().toString()+cb2.getSelectedItem().toString()+cb3.getSelectedItem().toString()
+					+" - "+textfield.getText();
 			
 			JTextField textField= new JTextField(select,10);
 			
