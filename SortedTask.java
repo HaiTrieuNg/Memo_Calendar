5ec9f88a8d7522e8b31b05c2f85d5e11c379e028
opalharshe <47206352+opalharshe@users.noreply.github.com>
2019-05-10 00:17:40 -0700

    Add files via upload
    The updated TaskFrame.java does not use Task.java class; instead it uses SortedTask.java class.  We should delete Task.java from final submission.

---
+++ SortedTask.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+
+class SortedTask extends AbstractListModel {
+
+  SortedSet model;
+
+  public SortedTask() {
+    model = new TreeSet();
+  }
+
+  public int getSize() {
+    return model.size();
+  }
+
+  public Object getElementAt(int index) {
+	    return model.toArray()[index];
+	  }
+
+  public void add(Object element) {
+    if (model.add(element)) {
+      fireContentsChanged(this, 0, getSize());
+    }
+  }
+
+  public void addAll(Object elements[]) {
+    Collection c = Arrays.asList(elements);
+    model.addAll(c);
+    fireContentsChanged(this, 0, getSize());
+  }
+
+  public void clear() {
+    model.clear();
+    fireContentsChanged(this, 0, getSize());
+  }
+
+  public boolean contains(Object element) {
+    return model.contains(element);
+  }
+
+  public Object firstElement() {
+    return model.first();
+  }
+
+  public Iterator iterator() {
+    return model.iterator();
+  }
+
+  public Object lastElement() {
+	    return model.last();
+	  }
+
+	public void delete(int i)
+	{
+		Object element = getElementAt(i);
+		model.remove(element);
+	    fireContentsChanged(this, 0, getSize());
+
+	}
+
+	public Object get(int i)
+	{
+	    return getElementAt(i);
+
+	}
+	
+  public boolean remove(Object element) {
+    boolean removed = model.remove(element);
+    if (removed) {
+      fireContentsChanged(this, 0, getSize());
+    }
+    return removed;
+  }
+
+}
